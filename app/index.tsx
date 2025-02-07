5da97dd5108566c264eb24cbd5a8d8e1114b7aa8
AntonyHROliveira <henritony73@gmail.com>
2025-02-07 11:57:18 -0300

    chore: atualização

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+      <StatusBar style="auto"></StatusBar>
     </View>
-  );
+  ); 
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#25292E",
+    alignItems:"center",
+    justifyContent:"center",
+  },
+  text:{
+    color:"#FFF",
+  },
+});
