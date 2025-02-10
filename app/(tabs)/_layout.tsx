fa14a500d817a5ef9ecc7199e7f4d15ff5688056
AntonyHROliveira <henritony73@gmail.com>
2025-02-10 11:35:10 -0300

    feat: create navigation tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,18 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      {/* <Stack.Screen name="index" options={{ title: "Home" }} /> */}
+      {/* <Stack.Screen name="index" options={{headerLeft: () => <></>, title: "Home" }}/> */}
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
+      {/* <Stack.Screen name="about" options={{ title: "About" }} /> */}
+      <Stack.Screen name="+not-found" />
+    </Stack>
+    /*Explicando: options={{headerLeft: () => <></>, title: "Home" }}
+    -> Para que quando estiver na página Home não aparecer a seta para retorna a outra página
+    */
+
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,40 @@
-import { Stack } from "expo-router";
+// import { Stack } from "expo-router";
+// import React from "react";
+
+// export default function RootLayout() {
+//   return (
+//     <Stack>
+//       {/* <Stack.Screen name="index" options={{ title: "Home" }} /> */}
+//       <Stack.Screen name="index" options={{headerLeft: () => <></>, title: "Home" }}/>
+//       <Stack.Screen name="about" options={{ title: "About" }} />
+//     </Stack>
+//     /*Explicando: options={{headerLeft: () => <></>, title: "Home" }}
+//     -> para que quando estiver na página Home não aparecer a seta para retorna a outra página
+//     */
+
+//   );
+// }
+
+
+
+
+
+// << 2° Versão >> //
+//================//
+import { Tabs } from "expo-router";
 import React from "react";
 
-export default function RootLayout() {
+// Criando navegação do tipo bar (Tabs)
+export default function TabLayout() {
   return (
-    <Stack>
-      {/* <Stack.Screen name="index" options={{ title: "Home" }} /> */}
-      <Stack.Screen name="index" options={{headerLeft: () => <></>, title: "Home" }}/>
-      <Stack.Screen name="about" options={{ title: "About" }} />
-    </Stack>
+    <Tabs>
+      {/* <Tabs.Screen name="index" options={{ title: "Home" }} /> */}
+      <Tabs.Screen name="index" options={{ title: "Home" }}/>
+      <Tabs.Screen name="about" options={{ title: "AboutS" }}/>
+    
+    </Tabs>
     /*Explicando: options={{headerLeft: () => <></>, title: "Home" }}
-    -> Para que quando estiver na página Home não aparecer a seta para retorna a outra página
+    -> para que quando estiver na página Home não aparecer a seta para retorna a outra página
     */
 
   );
