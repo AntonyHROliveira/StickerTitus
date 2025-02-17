690b5bdfd32b6ba8b83cdc29538c4c75ad934647
AntonyHROliveira <henritony73@gmail.com>
2025-02-17 10:55:19 -0300

    chore: upload assets

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -510,18 +510,21 @@
 
 
 import { View, StyleSheet } from "react-native";
-import ImageViewer from "@/components/ImageViewer";// <- são componentes personalizados localizados na pasta components do projeto.
-import Button from "@/components/Button";// <- são componentes personalizados localizados na pasta components do projeto.
-import * as ImagePicker from "expo-image-picker";// <- é uma biblioteca do Expo que permite selecionar imagens da galeria ou câmera do dispositivo.
 import { useState } from "react";// <-  é um hook do React para gerenciar estados em componentes funcionais.
-import IconButton from "@/components/IconButton";// <- são componentes personalizados localizados na pasta components do projeto.
-import CicleButton from "@/components/CicleButton";// <- são componentes personalizados localizados na pasta components do projeto.
-import EmojiPicker from "@/components/EmojiPicker";// <- são componentes personalizados localizados na pasta components do projeto.
+import * as ImagePicker from "expo-image-picker";// <- é uma biblioteca do Expo que permite selecionar imagens da galeria ou câmera do dispositivo.
+
+// imports dos componentes personalizados localizados na pasta components do projeto.
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+import IconButton from "@/components/IconButton";
+import CicleButton from "@/components/CicleButton";
+import EmojiPicker from "@/components/EmojiPicker";
 
 // 10° Versão //
 //====================================================================//
 const PlaceholderImage = require("@/assets/images/image_dog.jpg");// <- Carrega uma imagem local (image_dog.jpg) localizada na pasta assets/images
 
+// export default function Index(){} => Define um componente funcional padrão exportado.
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>( undefined );
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
