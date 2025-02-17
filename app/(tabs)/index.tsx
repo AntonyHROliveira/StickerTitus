e66c9bd5e9a4a07d9b4fbb695fdbee690d5662f1
AntonyHROliveira <henritony73@gmail.com>
2025-02-17 10:42:11 -0300

    chore: use EmojiPicker component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -510,22 +510,22 @@
 
 
 import { View, StyleSheet } from "react-native";
-import ImageViewer from "@/components/ImageViewer";
-import Button from "@/components/Button";
-import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
-import IconButton from "@/components/IconButton";
-import CicleButton from "@/components/CicleButton";
+import ImageViewer from "@/components/ImageViewer";// <- são componentes personalizados localizados na pasta components do projeto.
+import Button from "@/components/Button";// <- são componentes personalizados localizados na pasta components do projeto.
+import * as ImagePicker from "expo-image-picker";// <- é uma biblioteca do Expo que permite selecionar imagens da galeria ou câmera do dispositivo.
+import { useState } from "react";// <-  é um hook do React para gerenciar estados em componentes funcionais.
+import IconButton from "@/components/IconButton";// <- são componentes personalizados localizados na pasta components do projeto.
+import CicleButton from "@/components/CicleButton";// <- são componentes personalizados localizados na pasta components do projeto.
+import EmojiPicker from "@/components/EmojiPicker";// <- são componentes personalizados localizados na pasta components do projeto.
 
 // 10° Versão //
 //====================================================================//
-const PlaceholderImage = require("@/assets/images/image_dog.jpg");
+const PlaceholderImage = require("@/assets/images/image_dog.jpg");// <- Carrega uma imagem local (image_dog.jpg) localizada na pasta assets/images
 
 export default function Index() {
-  const [selectedImage, setSelectedImage] = useState<string | undefined>(
-    undefined
-  );
+  const [selectedImage, setSelectedImage] = useState<string | undefined>( undefined );
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   
 
   const pickImageAsync = async () => {
@@ -544,7 +544,8 @@ export default function Index() {
   };
 
  const onReset = () => {setShowAppOptions(false)};
- const onAddSticker = () => {};
+ const onAddSticker = () => {setIsModalVisible(true)};
+ const onModalChose = () => {setIsModalVisible(false)};
  const onSaveImageAsync = () => {};
 
   return (
@@ -579,6 +580,9 @@ export default function Index() {
           <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
         </View>
       )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalChose}>
+
+      </EmojiPicker>
     </View>
   );
 }
