aa418aff0d72ceec9cd6dce613b0c79e7a504e3b
AntonyHROliveira <henritony73@gmail.com>
2025-02-19 10:36:09 -0300

    chore: implemet pan gesture

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -20,10 +20,66 @@
 // }
 // //=================================================================================\\
 
+// // 2º Versão \\
+// //====================================================================================\\
+// import { type ImageSource } from "expo-image";
+// import { View } from "react-native";
+// import Animated, {
+//   useAnimatedStyle,
+//   useSharedValue,
+//   withSpring,
+// } from "react-native-reanimated";
+// import { Gesture, GestureDetector } from "react-native-gesture-handler"; // <= Biblioteca para jetos de (arrastar, ampliar, toque).
+
+// type Props = {
+//   imageSize: number;
+//   stickerource: ImageSource;
+// };
+
+// export default function EmojiSticker({ imageSize, stickerource }: Props) {
+
+//   const scaleImage = useSharedValue(imageSize);// <= const para aumentar ou diminuir o tamanho do emoji.
+//   // Gesture.Tap => é para dois toque
+//   const doubleTap = Gesture.Tap()
+//     .numberOfTaps(2)
+//     .onStart(() => {
+//       // if?else = (se o valor for diferente de 2) então aumenta o tamanho do emoji em 2X
+//       if (scaleImage.value !== imageSize * 2) {
+//         scaleImage.value = scaleImage.value * 2;
+//       } else {
+//         // Math arredonda o value.
+//         scaleImage.value = Math.round(scaleImage.value / 2);
+//       }
+//     });
+
+//   const imageStyle = useAnimatedStyle(() => {
+//     // return como {} retorna um objeto.
+//     return {
+//       width: withSpring(scaleImage.value),
+//       height: withSpring(scaleImage.value),
+//     };
+//   });
+
+//   return (
+//     <View style={{ top: -350 }}>
+//       <GestureDetector gesture={doubleTap}>// <= para trabalhar com toque
+//         <Animated.Image
+//           source={stickerource}
+//           style={[imageStyle, { width: imageSize, height: imageSize }]} // <= [] coloca o style dentro de um vetor
+//           resizeMode="contain"
+//         />
+//       </GestureDetector>
+//     </View>
+//   );
+// }
+// // ANOTAÇÕES:
+// // A tag <Animated> só funciona animado outras tags por isso usa assim <Animated.Image.
+// // No react-native todo o (use) lida com Hooks : EXEMPLO {useSharedValue}
+// //======================================================================================\\
+
 // 2º Versão \\
 //====================================================================================\\
 import { type ImageSource } from "expo-image";
-import { View } from "react-native";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -37,9 +93,29 @@ type Props = {
 };
 
 export default function EmojiSticker({ imageSize, stickerource }: Props) {
-  const scaleImage = useSharedValue(imageSize);
-  // Gesture.Tap => é para dois toque
-  const doubleTap = Gesture.Tap()
+  // (translateX & translatey) => para lidar com a posição do emoji
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  // função para lidar a posição do emoji.
+  const drag = Gesture.Pan().onChange((event) => {
+    translateX.value += event.changeX; // <= pega a posição inicial e soma a nova posição
+    translateY.value += event.changeY; // <= pega a posição inicial e soma a nova posição
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    };
+  });
+
+  const scaleImage = useSharedValue(imageSize); // <= const para aumentar ou diminuir o tamanho do emoji.
+
+  // função para lidar com o toque
+  const doubleTap = Gesture.Tap() // Gesture.Tap => é para dois toque
     .numberOfTaps(2)
     .onStart(() => {
       // if?else = (se o valor for diferente de 2) então aumenta o tamanho do emoji em 2X
@@ -60,15 +136,17 @@ export default function EmojiSticker({ imageSize, stickerource }: Props) {
   });
 
   return (
-    <View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image
-          source={stickerource}
-          style={[imageStyle, { width: imageSize, height: imageSize }]} // <= [] coloca o style dentro de um vetor
-          resizeMode="contain"
-        />
-      </GestureDetector>
-    </View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerource}
+            style={[imageStyle, { width: imageSize, height: imageSize }]} // <= [] coloca o style dentro de um vetor
+            resizeMode="contain"
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 }
 // ANOTAÇÕES:
