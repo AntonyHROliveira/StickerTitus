fb5e08869e36ce01e2b11fe42c6170406d84e6be
AntonyHROliveira <henritony73@gmail.com>
2025-02-19 10:06:52 -0300

    chore: implement zoom on double tap gesture

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -482,10 +482,144 @@
 // });
 // //====================================================================//
 
+// import { View, StyleSheet } from "react-native";
+// import { useState } from "react"; // <-  é um hook do React para gerenciar estados em componentes funcionais.
+// import * as ImagePicker from "expo-image-picker"; // <- é uma biblioteca do Expo que permite selecionar imagens da galeria ou câmera do dispositivo.
+// import { type ImageSource } from "expo-image";
+
+// // imports dos componentes personalizados localizados na pasta components do projeto.
+// import Button from "@/components/Button";
+// import CicleButton from "@/components/CicleButton";
+// import EmojiPicker from "@/components/EmojiPicker";
+// import EmojiList from "@/components/EmojiList";
+// import EmojiSticker from "@/components/EmojiSticker";
+// import ImageViewer from "@/components/ImageViewer";
+// import IconButton from "@/components/IconButton";
+
+// // 10° Versão //
+// //====================================================================//
+// const PlaceholderImage = require("@/assets/images/image_dog.jpg"); // <- Carrega uma imagem local (image_dog.jpg) localizada na pasta assets/images
+
+// // export default function Index(){} => Define um componente funcional padrão exportado.
+// // OBS: As const gerenciam os estados
+// export default function Index() {
+//   const [selectedImage, setSelectedImage] = useState<string | undefined>(
+//     undefined
+//   );
+//   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+//   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+//   const [pickedEmoji, setPickedEmoje] = useState<ImageSource | undefined>(
+//     undefined
+//   );
+
+//   const pickImageAsync = async () => {
+//     let result = await ImagePicker.launchImageLibraryAsync({
+//       mediaTypes: ["images"],
+//       allowsEditing: true,
+//       quality: 1,
+//     });
+
+//     if (!result.canceled) {
+//       setSelectedImage(result.assets[0].uri);
+//       setShowAppOptions(true);
+//     } else {
+//       alert("You did not select any image.");
+//     }
+//   };
+
+//   const onReset = () => {
+//     setShowAppOptions(false);
+//   };
+//   const onAddSticker = () => {
+//     setIsModalVisible(true);
+//   };
+//   const onModalChose = () => {
+//     setIsModalVisible(false);
+//   };
+//   const onSaveImageAsync = () => {};
+
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.imageContainer}>
+//         <ImageViewer
+//           imgSource={PlaceholderImage}
+//           selectedImage={selectedImage}
+//         />
+//         {pickedEmoji && (
+//           <EmojiSticker imageSize={40} stickerource={pickedEmoji} />
+//         )}
+//         {/* selectedImage={selectedImage} 
+//         selectedImage= =>  selectedImage?: string; da Prop da página ImageViewer.tsx
+//         {selectedImage} =>  const [selectedImage, setSelectedImage] é o selectedImage da const dessa página.*/}
+//       </View>
+
+//       {/*abrindo um ternario = {showAppOptions ? <View /> : } => para ocultar esta view  */}
+//       {showAppOptions ? (
+//         <View style={styles.optionsContainer}>
+//           <View style={styles.optionsRow}>
+//             <IconButton icon="refresh" label="Reset" onPress={onReset} />
+//             <CicleButton onPress={onAddSticker} />
+//             <IconButton
+//               icon="save-alt"
+//               label="Save"
+//               onPress={onSaveImageAsync}
+//             />
+//           </View>
+//         </View>
+//       ) : (
+//         <View style={styles.footerConatiner}>
+//           <Button
+//             label="Choose a photo"
+//             theme="primary"
+//             onPress={pickImageAsync}
+//           />
+//           {/*theme="primary" <- PARA colocar um style proprio neste botão*/}
+//           <Button
+//             label="Use this photo"
+//             onPress={() => setShowAppOptions(true)}
+//           />
+//         </View>
+//       )}
+//       <EmojiPicker isVisible={isModalVisible} onClose={onModalChose}>
+//         <EmojiList onSelect={setPickedEmoje} onCloseModal={onModalChose} />
+//       </EmojiPicker>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "#25292E",
+//   },
+//   imageContainer: {
+//     flex: 1,
+//     paddingTop: 28,
+//   },
+//   footerConatiner: {
+//     flex: 1 / 3, // <- para usar 33% da tela.
+//     alignItems: "center",
+//   },
+//   optionsContainer: {
+//     position: "absolute",
+//     bottom: 80,
+//   },
+//   optionsRow: {
+//     alignItems: "center",
+//     flexDirection: "row",
+//   },
+// });
+// //====================================================================//
+
+
+
 import { View, StyleSheet } from "react-native";
 import { useState } from "react"; // <-  é um hook do React para gerenciar estados em componentes funcionais.
 import * as ImagePicker from "expo-image-picker"; // <- é uma biblioteca do Expo que permite selecionar imagens da galeria ou câmera do dispositivo.
 import { type ImageSource } from "expo-image";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 // imports dos componentes personalizados localizados na pasta components do projeto.
 import Button from "@/components/Button";
@@ -496,7 +630,7 @@ import EmojiSticker from "@/components/EmojiSticker";
 import ImageViewer from "@/components/ImageViewer";
 import IconButton from "@/components/IconButton";
 
-// 10° Versão //
+// 11° Versão //
 //====================================================================//
 const PlaceholderImage = require("@/assets/images/image_dog.jpg"); // <- Carrega uma imagem local (image_dog.jpg) localizada na pasta assets/images
 
@@ -539,7 +673,7 @@ export default function Index() {
   const onSaveImageAsync = () => {};
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           imgSource={PlaceholderImage}
@@ -583,7 +717,7 @@ export default function Index() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalChose}>
         <EmojiList onSelect={setPickedEmoje} onCloseModal={onModalChose} />
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,5 +1,35 @@
-import { Image, type ImageSource } from "expo-image";
+// // 1º Versão \\
+// //=================================================================================\\
+// import { Image, type ImageSource } from "expo-image";
+// import { View } from "react-native";
+
+// type Props = {
+//   imageSize: number;
+//   stickerource: ImageSource;
+// };
+
+// export default function EmojiSticker({ imageSize, stickerource }: Props) {
+//   return (
+//     <View style={{ top: -350 }}>
+//       <Image
+//         source={stickerource}
+//         style={{ width: imageSize, height: imageSize }}
+//       />
+//     </View>
+//   );
+// }
+// //=================================================================================\\
+
+// 2º Versão \\
+//====================================================================================\\
+import { type ImageSource } from "expo-image";
 import { View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { Gesture, GestureDetector } from "react-native-gesture-handler"; // <= Biblioteca para jetos de (arrastar, ampliar, toque).
 
 type Props = {
   imageSize: number;
@@ -7,12 +37,41 @@ type Props = {
 };
 
 export default function EmojiSticker({ imageSize, stickerource }: Props) {
+  const scaleImage = useSharedValue(imageSize);
+  // Gesture.Tap => é para dois toque
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      // if?else = (se o valor for diferente de 2) então aumenta o tamanho do emoji em 2X
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
+      } else {
+        // Math arredonda o value.
+        scaleImage.value = Math.round(scaleImage.value / 2);
+      }
+    });
+
+  const imageStyle = useAnimatedStyle(() => {
+    // return como {} retorna um objeto.
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
+
   return (
     <View style={{ top: -350 }}>
-      <Image
-        source={stickerource}
-        style={{ width: imageSize, height: imageSize }}
-      />
+      <GestureDetector gesture={doubleTap}>
+        <Animated.Image
+          source={stickerource}
+          style={[imageStyle, { width: imageSize, height: imageSize }]} // <= [] coloca o style dentro de um vetor
+          resizeMode="contain"
+        />
+      </GestureDetector>
     </View>
   );
 }
+// ANOTAÇÕES:
+// A tag <Animated> só funciona animado outras tags por isso usa assim <Animated.Image.
+// No react-native todo o (use) lida com Hooks : EXEMPLO {useSharedValue}
+//======================================================================================\\
